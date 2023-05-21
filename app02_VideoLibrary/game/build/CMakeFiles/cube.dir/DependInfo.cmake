e62dd3cd68a5f73b6caec902620f9720a810392c
vctrubio <vctrubio@gmail.com>
2023-05-21 17:25:25 +0100

    problem inside mlx library so build beforehand

---
+++ app02_VideoLibrary/game/build/CMakeFiles/cube.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/calloc.c" "CMakeFiles/cube.dir/srcs/calloc.c.o" "gcc" "CMakeFiles/cube.dir/srcs/calloc.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/draw.c" "CMakeFiles/cube.dir/srcs/draw.c.o" "gcc" "CMakeFiles/cube.dir/srcs/draw.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/inits.c" "CMakeFiles/cube.dir/srcs/inits.c.o" "gcc" "CMakeFiles/cube.dir/srcs/inits.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/keys.c" "CMakeFiles/cube.dir/srcs/keys.c.o" "gcc" "CMakeFiles/cube.dir/srcs/keys.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/main.c" "CMakeFiles/cube.dir/srcs/main.c.o" "gcc" "CMakeFiles/cube.dir/srcs/main.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/map.c" "CMakeFiles/cube.dir/srcs/map.c.o" "gcc" "CMakeFiles/cube.dir/srcs/map.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/map_creation.c" "CMakeFiles/cube.dir/srcs/map_creation.c.o" "gcc" "CMakeFiles/cube.dir/srcs/map_creation.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/move.c" "CMakeFiles/cube.dir/srcs/move.c.o" "gcc" "CMakeFiles/cube.dir/srcs/move.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/parse.c" "CMakeFiles/cube.dir/srcs/parse.c.o" "gcc" "CMakeFiles/cube.dir/srcs/parse.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/pixel_manipulation.c" "CMakeFiles/cube.dir/srcs/pixel_manipulation.c.o" "gcc" "CMakeFiles/cube.dir/srcs/pixel_manipulation.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/static.c" "CMakeFiles/cube.dir/srcs/static.c.o" "gcc" "CMakeFiles/cube.dir/srcs/static.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/static_rtn.c" "CMakeFiles/cube.dir/srcs/static_rtn.c.o" "gcc" "CMakeFiles/cube.dir/srcs/static_rtn.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/util_convert.c" "CMakeFiles/cube.dir/srcs/util_convert.c.o" "gcc" "CMakeFiles/cube.dir/srcs/util_convert.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/util_gnl.c" "CMakeFiles/cube.dir/srcs/util_gnl.c.o" "gcc" "CMakeFiles/cube.dir/srcs/util_gnl.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/util_list.c" "CMakeFiles/cube.dir/srcs/util_list.c.o" "gcc" "CMakeFiles/cube.dir/srcs/util_list.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/util_print.c" "CMakeFiles/cube.dir/srcs/util_print.c.o" "gcc" "CMakeFiles/cube.dir/srcs/util_print.c.o.d"
+  "/Users/trtp/github/CommonMS_Prototype/app02_VideoLibrary/game/srcs/util_str.c" "CMakeFiles/cube.dir/srcs/util_str.c.o" "gcc" "CMakeFiles/cube.dir/srcs/util_str.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
